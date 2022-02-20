9ca35f4fcc26e4758a5ef5754376532789173bd4
AndrejApa <81564427+AndrejApa@users.noreply.github.com>
2022-02-20 14:37:37 +0300

    v

--- solution.go
+++ solution.go
@@ -20,11 +20,11 @@ type sidesCount int
 
 func CalcSquare(sideLen float64, sidesNum sidesCount) float64 {
 	switch sidesNum {
-	case 4:
+	case SidesSquare:
 		return math.Pow(sideLen, 2)
-	case 3:
+	case SidesTriangle:
 		return math.Pow(sideLen, 2) * math.Sqrt(sideLen) / 4
-	case 0:
+	case SidesCircle:
 		return math.Pi * math.Pow(sideLen, 2)
 	default:
 		return 0
